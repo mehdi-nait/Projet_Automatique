f97a7aafe294995db813039e7fe2af30abe62938
mehdi-nait <mehdi.nait.hammou1@gmail.com>
2021-06-01 11:47:07 +0200

    ajout asservissement

--- test_asserv/test_asserv.ino
+++ test_asserv/test_asserv.ino
@@ -13,13 +13,13 @@ int EN_A = 10;
 
 //Variables asservissement
 
-float diff=0,diffa=0,diffd=0;
-int Consigne = 50;
+float diff=0,diffa=0,diffd=0,diffPrec=0;
+int consigne = 50;
 int dt = 1;
 
-float Kp,Ki,Kd;
+float Kp=0.6106,Ki=184.1,Kd=0;
 
-
+int commande_EnA;
 void setup() {
   // put your setup code here, to run once:
 
@@ -33,19 +33,19 @@ void setup() {
 
   Serial.begin(9600);
 
-  //préparation export excel
-  Serial.println("CLEARDATA"); 
-  Serial.println("LABEL,temp");
-  Serial.println("RESETTIMER");
+//  //préparation export excel
+//  Serial.println("CLEARDATA"); 
+//  Serial.println("LABEL,temp");
+//  Serial.println("RESETTIMER");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  diff=0,diffa=0,diffd=0,diffPrec=0;
 //Mise en marche de la résistance chauffante
   digitalWrite(IN_1,HIGH);
   digitalWrite(IN_2,LOW);
-  analogWrite(EN_A,255);
+  
 //Calcul de la température avec la thermistance
   val = analogRead(pin_therm);  // read the input pin
   tension = (val*3.3)/676;
@@ -54,18 +54,36 @@ void loop() {
   temp = 3.8997*pow(tension,3) - 17.089*pow(tension,2)+ 49.686*tension - 43.095;
   
   //Serial.println(temp);
-
-  diff  = temp-consigne;
+  //temp = 25;
+  diffPrec = diff;
+  diff  = consigne-temp;
   diffa = diffa+diff;   //intégration
   diffd = diffd - diff; //dérivée
+  //diffd = diff - diffPrec; //dérivée
   
-  commande_EnA = Kp*diff+Ki+diffa*dt;
+  commande_EnA = Kp*diff+Ki*diffa*dt+Kd*diffd*dt;
 
   if(commande_EnA<0){
-      //chauffer
-      commande_E
+    commande_EnA = 0;
   }
+  commande_EnA = map(commande_EnA, 0, 4500, 0, 255);
+
+  if(commande_EnA>255){
+    commande_EnA = 255;
+  }
+
+  analogWrite(EN_A,commande_EnA);
+  Serial.print("temp : ");
+  Serial.print(temp);
+  Serial.print("\t commande : ");
+  Serial.print(commande_EnA);
+  Serial.print("\t diff : ");
+  Serial.print(diff);
+  Serial.print("\t diffD : ");
+  Serial.print(diffd);
+  Serial.print("\t diffA: ");
+  Serial.println(diffa);
   
   
-  //delay(1000);
+  delay(500);
 }
